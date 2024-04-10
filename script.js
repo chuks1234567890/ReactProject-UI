9f73065017f11d06acabc2104941964ee59f1a4b
Augustine Godwin <129863865+chuks1234567890@users.noreply.github.com>
2024-04-10 12:57:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-var boxes =document.querySelectorAll(".div .dd1")
-for (let i = 0; i <boxes.length; i++) {
-    let randomHex = Math.floor(Math.random() * 0xffffff).toString(16);
-    randomHex = `#${randomHex.padStart(6, "0")}`;
-    boxes[i].style.backgroundColor=randomHex
-    
-} 
 const home=document.querySelector(".content-section")
 const torn =document.querySelector(".tournament-page")
 const links=document.querySelectorAll(".nav-section ul li")
